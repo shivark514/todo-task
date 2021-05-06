3fe8e4cf110e13e822c3ae5461d05862549750bc
Perugu swathi <Peruguswathi11@gmail.com>
2021-05-06 15:47:18 +0530

    added log

--- componets/common.sh
+++ componets/common.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+Head() {
+  echo -e "\e[1;36m=======================$1=================\e[0m"
+}
+
 OS_PREREQ() {
   LOG=/tmp/${COMPONENT}.log
   rm -f $LOG
    set-hostname $(COMPONENT)
+   Head "Updating APT Repos"
    apt update &>$LOG
 }
 ERROR() {
