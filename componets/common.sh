8b7a7d7d65828c2bb9433d3ca8d12ef62cefe1ea
Perugu swathi <Peruguswathi11@gmail.com>
2021-05-06 16:25:44 +0530

    #3

--- componets/common.sh
+++ componets/common.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
- HEAD
+
+LOG=/tmp/${COMPONENT}.log
+rm -f $LOG
+
 Head() {
   echo -e "\e[1;36m=======================$1=================\e[0m"
+  echo -e "\e[1;36m=======================$1=================\e[0m">>$LOG
 }
 
 OS_PREREQ() {
-  LOG=/tmp/${COMPONENT}.log
-  rm -f $LOG
    set-hostname $(COMPONENT)
    Head "Updating APT Repos"
    apt update &>$LOG
-OS_PREREQ() {
-   set-hostname $(COMPONENT)
-   apt update
-origin/main
 }
 ERROR() {
   echo -e "\e[1;31m$1\e[0m"
