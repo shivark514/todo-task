236a6ec55ec577805e86bc91a014f35fa94b52c6
Perugu swathi <Peruguswathi11@gmail.com>
2021-05-07 14:48:23 +0530

    #7

--- componets/frontend.sh
+++ componets/frontend.sh
@@ -9,18 +9,18 @@ Head "Install Nginx"
 apt install nginx -y >>"${LOG}"
 Head "Start Nginx"
 systemctl start nginx
-Head "Install Node & Nginx"
-NPM
-STAT $?
+Head "Install Node "
+npm install >>"${LOG}"
+Stat $?
 
 Head "Change directory and make vue directory and switch to vue directory"
 cd /var/www/html && mkdir vue &&
 cd vue || exit
-STAT $?
+Stat $?
 
 Head "Clone code from Github"
 git clone https://github.com/zelar-soft-todoapp/frontend.git &>>$LOG && cd frontend
-STAT $?
+Stat $?
 
 Head "Install Npm"
 npm install >>"${LOG}"
@@ -30,23 +30,23 @@ Head "Run build"
 npm run build >>"${LOG}"
 Killall node >>"${LOG}"
 npm run build
-STAT $?
+Stat $?
 
 Head "Change root path in nginx"
 cd /etc/nginx/sites-available || exit
 vi default
-STAT $?
+Stat $?
 
 Head "Update index.js file with todo app & login ip"
 cd /var/www/html/vue/frontend &&
 cd config || exit
 vi index.js
-STAT $?
+Stat $?
 
 Head "Restart Nginx"
 systemctl restart nginx
-STAT $
+Stat $
 
 Head "run Npm Service"
 npm start
-STAT $?
+Stat $?
