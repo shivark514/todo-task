7cfde5eb012bbba6b884a208ffdc937add56457c
Perugu swathi <Peruguswathi11@gmail.com>
2021-05-10 13:25:57 +0530

    #19

--- components/login.sh
+++ components/login.sh
@@ -12,16 +12,16 @@ cd /root/
 git clone https://github.com/swathi-zelarsoft680/login.git&>>$LOG && cd login
 
 
-go build
+Head "Building Package"
+go build &>>$LOG
 
 Head "Get dependencies"
 go get github.com/dgrijalva/jwt-go &>>$LOG && go get github.com/labstack/echo &>>$LOG && go get github.com/labstack/echo/middleware &>>$LOG && go get github.com/labstack/gommon/log &>>$LOG && go get github.com/openzipkin/zipkin-go &>>$LOG && go get github.com/openzipkin/zipkin-go/middleware/http &>>$LOG && go get github.com/openzipkin/zipkin-go/reporter/http &>>$LOG
 Stat $?
 
-Head "Building Package"
-go build &>>$LOG
-Stat $?
-rm -rf /etc/systemd/system/login.service
+go build main.go user.go tracing.go &>>$LOG
+
+#rm -rf /etc/systemd/system/login.service
 mv systemd.service /etc/systemd/system/login.service
 Stat $?
 Head "Restarting Services"
