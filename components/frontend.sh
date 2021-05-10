c97bf4c57bd56a8bd7db295531a6530a48c1ee57
Perugu swathi <Peruguswathi11@gmail.com>
2021-05-10 13:16:30 +0530

    #16

--- components/frontend.sh
+++ components/frontend.sh
@@ -20,10 +20,14 @@ npm install &>>$LOG && npm run build &>>$LOG
 Head "Moving Conf file"
 sed -i -e 's+/var/www/html+/var/www/html/frontend/dist+g' /etc/nginx/sites-enabled/default
 Stat $?
-
 Head "Exporting Ip's"
 export AUTH_API_ADDRESS=http://172.31.24.10:8080
 export TODOS_API_ADDRESS=http://172.31.21.55:8080
+
+Head "Restart nginx"
+systemctl restart nginx
+systemctl enable nginx
+
 
 
 Head "Starting Npm Service"
