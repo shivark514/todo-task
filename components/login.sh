58591e24096b14ab88eee02c0db83f1223f2e9b9
Perugu swathi <Peruguswathi11@gmail.com>
2021-05-10 13:27:43 +0530

    #20

--- components/login.sh
+++ components/login.sh
@@ -18,12 +18,12 @@ go build &>>$LOG
 Head "Get dependencies"
 go get github.com/dgrijalva/jwt-go &>>$LOG && go get github.com/labstack/echo &>>$LOG && go get github.com/labstack/echo/middleware &>>$LOG && go get github.com/labstack/gommon/log &>>$LOG && go get github.com/openzipkin/zipkin-go &>>$LOG && go get github.com/openzipkin/zipkin-go/middleware/http &>>$LOG && go get github.com/openzipkin/zipkin-go/reporter/http &>>$LOG
 Stat $?
-
+Head "Again Build"
 go build main.go user.go tracing.go &>>$LOG
 
 #rm -rf /etc/systemd/system/login.service
-mv systemd.service /etc/systemd/system/login.service
-Stat $?
+#mv systemd.service /etc/systemd/system/login.service
+
 Head "Restarting Services"
 systemctl daemon-reload &>>$LOG && systemctl start login && systemctl enable login &>>$LOG
 systemctl status login
